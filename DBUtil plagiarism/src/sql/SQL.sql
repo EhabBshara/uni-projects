2d330b707e9fe28704a73c08ae701ef8a8510233
AliWAA <AliWAA@Aliwassouf>
2016-05-22 11:56:29 -0700

    adding sql schema

--- DBUtil plagiarism/src/sql/SQL.sql
+++ DBUtil plagiarism/src/sql/SQL.sql
@@ -1,8 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2016 at 06:52 PM
+-- Server version: 5.7.9
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `plagiarism`
 --
 CREATE DATABASE IF NOT EXISTS `plagiarism` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `plagiarism`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `annotation`
+--
+
+DROP TABLE IF EXISTS `annotation`;
+CREATE TABLE IF NOT EXISTS `annotation` (
+  `annotation_id` int(11) NOT NULL,
+  `source_offset` mediumtext,
+  `source_length` mediumtext,
+  `suspiciuos_offset` mediumtext,
+  `suspiciuos_length` mediumtext,
+  `obfuscation` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `source_doc_id` int(11) DEFAULT NULL,
+  `suspiciuos_doc_id` int(11) NOT NULL,
+  PRIMARY KEY (`annotation_id`),
+  KEY `fk_annotation_source_doc_idx` (`source_doc_id`),
+  KEY `fk_annotation_suspicious_doc1_idx` (`suspiciuos_doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -19,14 +59,7 @@ CREATE TABLE IF NOT EXISTS `assoc` (
   PRIMARY KEY (`assoc_id`),
   KEY `fk_assoc_phrase1_idx` (`phrase_id`),
   KEY `fk_assoc_testphrase1_idx` (`testphrase_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `assoc`
---
-
-INSERT INTO `assoc` (`assoc_id`, `score`, `phrase_id`, `testphrase_id`) VALUES
-(1, 99, 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,20 +72,40 @@ CREATE TABLE IF NOT EXISTS `phrase` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pathname` varchar(200) DEFAULT NULL,
   `filename` varchar(200) DEFAULT NULL,
-  `original` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+  `original` text,
+  `tokens` text,
+  `source_doc_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_phrase_source_doc1_idx` (`source_doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `phrase`
+-- Table structure for table `source_doc`
 --
 
-INSERT INTO `phrase` (`id`, `pathname`, `filename`, `original`) VALUES
-(1, 'Ali wassouf', 'Aliano Paco', 'I hat you '),
-(4, NULL, NULL, NULL),
-(13, NULL, NULL, NULL),
-(14, NULL, NULL, NULL),
-(15, NULL, NULL, NULL);
+DROP TABLE IF EXISTS `source_doc`;
+CREATE TABLE IF NOT EXISTS `source_doc` (
+  `source_doc_id` int(11) NOT NULL,
+  `source_doc_text` text,
+  `source_doc_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`source_doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suspiciuos_doc`
+--
+
+DROP TABLE IF EXISTS `suspiciuos_doc`;
+CREATE TABLE IF NOT EXISTS `suspiciuos_doc` (
+  `suspiciuos_doc_id` int(11) NOT NULL,
+  `suspiciuos_doc_text` text,
+  `suspiciuos_doc_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`suspiciuos_doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -66,46 +119,21 @@ CREATE TABLE IF NOT EXISTS `testphrase` (
   `filename` varchar(200) DEFAULT NULL,
   `pathname` varchar(200) DEFAULT NULL,
   `phrase` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `testphrase`
---
-
-INSERT INTO `testphrase` (`id`, `filename`, `pathname`, `phrase`) VALUES
-(1, 'Aliano', 'Georgina', 'I love you');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `token`
---
-
-DROP TABLE IF EXISTS `token`;
-CREATE TABLE IF NOT EXISTS `token` (
-  `token_id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` varchar(45) DEFAULT NULL,
-  `phrase_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`token_id`),
-  KEY `fk_phrase_id` (`phrase_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `token`
---
-
-INSERT INTO `token` (`token_id`, `content`, `phrase_id`) VALUES
-(10, 'Ali', 14),
-(11, 'sam', 14),
-(12, 'Ehab', 14),
-(13, 'Ali', 15),
-(14, 'sam', 15),
-(15, 'Ehab', 15);
+  `suspicious_doc_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_testphrase_suspicious_doc1_idx` (`suspicious_doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `annotation`
+--
+ALTER TABLE `annotation`
+  ADD CONSTRAINT `fk_annotation_source_doc` FOREIGN KEY (`source_doc_id`) REFERENCES `source_doc` (`source_doc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_annotation_suspicious_doc1` FOREIGN KEY (`suspiciuos_doc_id`) REFERENCES `suspiciuos_doc` (`suspiciuos_doc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `assoc`
@@ -115,7 +143,17 @@ ALTER TABLE `assoc`
   ADD CONSTRAINT `fk_assoc_testphrase1` FOREIGN KEY (`testphrase_id`) REFERENCES `testphrase` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `token`
+-- Constraints for table `phrase`
 --
-ALTER TABLE `token`
-  ADD CONSTRAINT `fk_phrase_id` FOREIGN KEY (`phrase_id`) REFERENCES `phrase` (`id`);
+ALTER TABLE `phrase`
+  ADD CONSTRAINT `fk_phrase_source_doc1` FOREIGN KEY (`source_doc_id`) REFERENCES `source_doc` (`source_doc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `testphrase`
+--
+ALTER TABLE `testphrase`
+  ADD CONSTRAINT `fk_testphrase_suspicious_doc1` FOREIGN KEY (`suspicious_doc_id`) REFERENCES `suspiciuos_doc` (`suspiciuos_doc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
