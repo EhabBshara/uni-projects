bb5812b07c43ed3a7b85024239ac149479a34c5e
mhd_dll <dali.mhmd@gmail.com>
2016-07-03 19:02:44 +0300

    adding all classes to evaluate algorithm

--- DBUtil plagiarism/src/Main/Main.java
+++ DBUtil plagiarism/src/Main/Main.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import plagiarism.DAOImpl.GenericServiceImpl;
 import plagiarism.IDAO.IGenericService;
 import plagiarism.util.pojos.Annotation;
@@ -17,6 +20,8 @@ import plagiarism.util.pojos.Assoc;
 import plagiarism.util.pojos.CandidateDocs;
 import plagiarism.util.pojos.HibernateUtil;
 import plagiarism.util.pojos.Phrase;
+import plagiarism.util.pojos.Source_doc;
+import plagiarism.util.pojos.Suspicious_doc;
 
 /**
  *
@@ -25,6 +30,43 @@ import plagiarism.util.pojos.Phrase;
 public class Main {
 
     public static void main(String[] args) {
+
+//        String myquery = "SELECT  a.annotation_id ,  a.source_offset ,  a.source_length ,  a.suspicious_offset ,  a.suspicious_length ,  a.obfuscation ,  a.type , MAX(  a.source_doc ) AS  source_doc , MAX(  a.suspicious_doc ) AS suspicious_doc "
+//                + "FROM Annotation as a "
+//                + "GROUP BY  a.suspicious_doc ,  a.source_doc ";
+//
+//        IGenericService<Annotation> annotationService
+//                = new GenericServiceImpl<>(Annotation.class, HibernateUtil.getSessionFactory());
+//        Map<String, Object> params1 = new HashMap<>();
+//        List<Annotation> annotations = (List<Annotation>) annotationService.query(myquery, null, null);
+
+        String hsql = "SELECT distinct  a.suspicious_doc , a.source_doc from Annotation a ";
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+//        Query query = session.createQuery(myquery);
+
+        List<Object[]> tuples = (List<Object[]>) session.createQuery(hsql).list();
+        session.getTransaction().commit();
+//        for (Object[] tuple : tuples) {
+//            Source_doc ue = (Source_doc) tuple[1];
+//            Suspicious_doc roleId = (Suspicious_doc) tuple[0];
+//        }
+
+        Evaluater evaluater = new Evaluater();
+        test t = new test();
+        List<Assoc> assocs = new ArrayList<>();
+        for (int a = 0; a < tuples.size(); a++) {
+            Source_doc source_doc = (Source_doc) tuples.get(a)[1];
+            Suspicious_doc suspicious_doc = (Suspicious_doc) tuples.get(a)[0];
+            List<CandidatePair> candidateSentences = t.assosiatePairs(source_doc, suspicious_doc);
+            for (CandidatePair pair : candidateSentences) {
+                if (evaluater.evaluate(pair.getPhrase(), pair.getTestPhrase(), true, false)) {
+                    assocs.add(new Assoc(pair.getPhrase(), pair.getTestPhrase(), 0.0));
+                }
+            }
+        }
 
 //                IGenericService<Annotation> annotationService
 //                = new GenericServiceImpl<>(Annotation.class, HibernateUtil.getSessionFactory());
@@ -35,47 +77,27 @@ public class Main {
 //            System.out.println(a.getSource_doc().getSource_doc_text().substring((int) a.getSource_offset(), (int) a.getSource_offset() + (int) a.getSource_length()));
 //            System.out.println("");
 //            System.out.println(a.getSuspicious_doc().getSuspicious_doc_text().substring((int) a.getSuspicious_offset(), (int) a.getSuspicious_offset() + (int) a.getSuspicious_length()));
-        IGenericService<CandidateDocs> candidateDocsService
-                = new GenericServiceImpl<>(CandidateDocs.class, HibernateUtil.getSessionFactory());
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("correlation_value", 0.14f);
-//        params.put("suspicious_doc_id", 339);
-        params.put("min", 169);
-        params.put("max", 339);
-        List<CandidateDocs> candidates = candidateDocsService.getByWhere("where correlation_value>:correlation_value and suspicious_doc_id<:max and suspicious_doc_id>:min", params);
-        test t = new test();
-        Evaluater evaluater = new Evaluater();
-        List<Assoc> assocs = new ArrayList<>();
-        for (int i = 0; i < candidates.size(); i++) {
-            CandidateDocs candidate = candidates.get(i);
-//        List<CandidatePair> sentencesPairsList = t.getCandidatePair(candidates.getSource_doc(), candidates.getSuspicious_doc());
-            List<CandidatePair> sentencesPairsList = t.assosiatePairs(candidate.getSource_doc(), candidate.getSuspicious_doc());
-            for (CandidatePair pair : sentencesPairsList) {
-                if (evaluater.evaluate(pair.getPhrase(), pair.getTestPhrase(),false,false)) {
-                    assocs.add(new Assoc(pair.getPhrase(), pair.getTestPhrase(), 0.0));
-                }
-            }
-        }
-
+//        test t = new test();
+//        Evaluater evaluater = new Evaluater();
+//        List<Assoc> assocs = new ArrayList<>();
+//        for (int i = 0; i < candidates.size(); i++) {
+//            CandidateDocs candidate = candidates.get(i);
+////        List<CandidatePair> sentencesPairsList = t.getCandidatePair(candidates.getSource_doc(), candidates.getSuspicious_doc());
+//            List<CandidatePair> sentencesPairsList = t.assosiatePairs(candidate.getSource_doc(), candidate.getSuspicious_doc());
+//            for (CandidatePair pair : sentencesPairsList) {
+//                if (evaluater.evaluate(pair.getPhrase(), pair.getTestPhrase(),false,false)) {
+//                    assocs.add(new Assoc(pair.getPhrase(), pair.getTestPhrase(), 0.0));
+//                }
+//            }
+//        }
+//
         IGenericService<Assoc> assocDocsService
                 = new GenericServiceImpl<>(Assoc.class, HibernateUtil.getSessionFactory());
-        assocDocsService.bulkSave(assocs);
+//        assocDocsService.bulkSave(assocs);
 //        sentencesPairsList = t.calculateFeatures(sentencesPairsList);
 //
 //        
 ////        System.out.println(sentencesPairsList);
-//        for (CandidatePair pair : sentencesPairsList) {
-//            if (pair.getLSC() > Constants.LCS_COEF) {
-//                System.out.println("LCS:\n"+pair.getPhrase().getOriginal() + "\n" + pair.getTestPhrase().getPhrase() + "\n-------");
-//            }
-//            if (pair.getSkipgram2() > Constants.SKIP_GRAM2_COEF) {
-//                System.out.println("skip2:\n"+pair.getPhrase().getOriginal() + "\n" + pair.getTestPhrase().getPhrase() + "\n-------");
-//            }
-//            if (pair.getSkipgram3() > Constants.SKIP_GRAM3_COEF) {
-//                System.out.println("skip3:\n"+pair.getPhrase().getOriginal() + "\n" + pair.getTestPhrase().getPhrase() + "\n-------");
-//            }
-//
-//        }
         System.out.println("done");
     }
 
---
+++ DBUtil plagiarism/src/Main/XMLResultsWriter.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Utils.Helpers;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import machineLearning.Features;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import plagiarism.DAOImpl.GenericServiceImpl;
+import plagiarism.IDAO.IGenericService;
+import plagiarism.util.pojos.HibernateUtil;
+import plagiarism.util.pojos.Suspicious_doc;
+
+/**
+ *
+ * @author dali
+ */
+public class XMLResultsWriter {
+
+    public static void main(String[] args) {
+        XMLResultsWriter XMLwriter = new XMLResultsWriter();
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        IGenericService<Suspicious_doc> suspiciousDocService
+                = new GenericServiceImpl<>(Suspicious_doc.class, HibernateUtil.getSessionFactory());
+        List<Suspicious_doc> susps = suspiciousDocService.getAll();
+
+        for (int i = 0; i < susps.size(); i++) {
+            Suspicious_doc suspicious_doc = susps.get(i);
+            String hsql = "SELECT sus.suspicious_doc_name , t.offset , t.length , s.source_doc_name , p.offset , p.length "
+                    + "FROM  Assoc AS assoc "
+                    + "INNER JOIN  assoc.phrase as p  "
+                    + "INNER JOIN p.source_doc as s "
+                    + "INNER JOIN  assoc.testphrase as t "
+                    + "INNER JOIN t.suspicious_doc as sus "
+                    + "where sus.suspicious_doc_id=" + suspicious_doc.getSuspicious_doc_id();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            List<Object[]> results = (List<Object[]>) session.createQuery(hsql).list();
+            session.getTransaction().commit();
+
+            
+            String outputXML = "";
+            if (results.size() == 0) {
+                outputXML = XMLwriter.justFirstRow(suspicious_doc.getSuspicious_doc_name()) + "\n";
+                XMLwriter.writeXMLFile(outputXML, "D:\\results\\" + suspicious_doc.getSuspicious_doc_name().substring(0, suspicious_doc.getSuspicious_doc_name().length() - 4) + ".xml");
+                continue;
+            }
+            String XML_name = (String) results.get(0)[0];
+            outputXML = XMLwriter.firstRow(XML_name) + "\n";
+
+            for (int j = 0; j < results.size(); j++) {
+                int susoffset = (int) results.get(j)[1];
+                int suslength = (int) results.get(j)[2];
+                String sourcename = (String) results.get(j)[3];
+                int sourceoffset = (int) results.get(j)[4];
+                int sourcelength = (int) results.get(j)[5];
+                outputXML += XMLwriter.featureExtracter(susoffset, suslength, sourcename, sourceoffset, sourcelength);
+            }
+
+            outputXML += "</document> ";
+            XMLwriter.writeXMLFile(outputXML, "D:\\results\\" + XML_name.substring(0, XML_name.length() - 4) + ".xml");
+        }
+        System.out.println("done");
+    }
+
+    String firstRow(String name) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<document reference=\"" + name + "\">";
+    }
+
+    String justFirstRow(String name) {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + "<document reference=\"" + name + "\"/>";
+    }
+
+    String featureExtracter(int this_offset, int this_length, String source_reference, int source_offset, int source_length) {
+        return "<feature name=\"detected-plagiarism\" this_offset=\"" + this_offset + "\" this_length=\"" + this_length + "\" source_reference=\"" + source_reference + "\" source_offset=\"" + source_offset + "\"   source_length=\"" + source_length + "\"  />\n";
+    }
+
+    public void writeXMLFile(String text, String path) {
+        BufferedWriter writer = null;
+        try {
+            //create a temporary file
+            File logFile = new File(path);
+            // This will output the full path where the file will be written to...
+            System.out.println(logFile.getCanonicalPath());
+            writer = new BufferedWriter(new FileWriter(logFile));
+            writer.write(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
